948a78d7f55452c31207ab86b1b4db689ce88bb1
Aki Sakata <akisoqls@gmail.com>
2024-02-11 15:47:54 +0900

    Add glb file to header

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,14 +1,22 @@
 import * as THREE from "three";
+import { GLTFLoader, type GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+
+const canvas = {
+  height: 100,
+  width: 100
+}
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 20, 70 / 30, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(50, canvas.height / canvas.width, 0.1);
+
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 let scrollX = 0.00
 let scrollY = 0.00
 let prevScrollPosition = 0;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( 70, 30 );
+renderer.setSize( canvas.height, canvas.width );
 document.querySelector("#icon")?.appendChild(renderer.domElement);
 window.addEventListener("scroll", () => {
   if (prevScrollPosition < window.scrollY) {
@@ -19,10 +27,32 @@ window.addEventListener("scroll", () => {
   prevScrollPosition = window.scrollY;
   
 })
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+const loader = new GLTFLoader();
+// GLTFファイルのパスを指定
+const glb = await loader.loadAsync('../assets/globs/pcNetwork.glb');
+
+const model = glb.scene;
+console.log(model)
+scene.add(model);
+
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
+directionalLight.position.set(-5, 5, 3)
+scene.add(directionalLight)
+
+renderer.setClearColor(0x000000, 0);
+
+// loader.load( '../assets/globs/icon.glb', function ( gltf ) {
+
+// 	scene.add( gltf.scene );
+
+// }, undefined, function ( error ) {
+
+// 	console.error( error );
+
+// } );
+// scene.add( cube );
 
 camera.position.z = 5;
 
@@ -30,11 +60,11 @@ function animate() {
   requestAnimationFrame(animate);
   
   scrollX -= 0.01;
-  cube.rotation.x = scrollX
+  model.rotation.x = scrollX
   scrollY -= 0.01;
-  cube.rotation.y = scrollY;
+  model.rotation.y = scrollY;
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 animate();
 
