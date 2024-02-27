338f86c3dd7e20c38fa1a7f93b32608eb2716042
Aki Sakata <akisoqls@gmail.com>
2024-02-28 02:36:11 +0900

    Add vite and configure for vite

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig, normalizePath } from "vite";
+import autoprefixer from "autoprefixer";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+  root: "./src",
+  server: {
+    port: 8082,
+    open: true
+  },
+  build: {
+    sourcemap: false,
+    outDir: "../public",
+  },
+  mode: "production",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./assets/gltf/*",
+          dest: "./assets/gltf"
+        }
+      ]
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: [
+            'last 2 versions',
+            'ie > 11',
+            'iOS >= 10',
+            'Android >= 5'
+          ]
+        })
+      ],
+    },
+  },
+});
