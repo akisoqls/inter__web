7a9f61b185011f22da83f5615127eda4c05ca7a9
Aki Sakata <akisoqls@gmail.com>
2024-02-28 12:52:26 +0900

    Add type guard

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-type artistProfile = {
+export type artistProfile = {
   artistName: string,
   links?: {
     name: string,
@@ -6,4 +6,43 @@ type artistProfile = {
     label: string
   }[]
   bio: string
+}
+
+export const isValidArtistProfile = (profileData: unknown): profileData is artistProfile => {
+  
+  if (
+    !(typeof profileData === "object") ||
+    profileData === null  
+  ) return false;
+
+  const hasArtistName = ("artistName" in profileData)
+    && (typeof profileData.artistName === "string");
+  
+  const hasLink = ("links" in profileData)
+    ? (
+      Array.isArray(profileData.links)
+      && profileData.links.every(link => isValidLink(link))
+    )
+    : true;
+  
+  const hasBio = ("bio" in profileData)
+    && (typeof profileData.bio === "string");
+
+  return hasArtistName && hasLink && hasBio;
+
+}
+
+const isValidLink = (linkObj: unknown): linkObj is artistProfile["links"] => {
+  
+  if (
+    !(typeof linkObj === "object") ||
+    !(linkObj !== null)
+  ) return false;
+  
+  const hasName = "name" in linkObj && typeof linkObj.name === "string";
+  const hasLink = "link" in linkObj && typeof linkObj.link === "string";
+  const hasLabel = "label" in linkObj && typeof linkObj.label === "string";
+  
+  return hasName && hasLink && hasLabel;
+  
 }
