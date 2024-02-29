07132f62a976043f160643e5728a47d4a9b9a99e
Aki Sakata <akisoqls@gmail.com>
2024-02-29 11:17:56 +0900

    [WIP] Update to new web design

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,15 +5,20 @@ import { isValidArtistProfile } from "../types.ts";
     const jsonRes = await window.fetch("../assets/jsons/artists.json");
     const maybeArtistsObj: unknown[] = await jsonRes.json();
     
-    const innerHtml = maybeArtistsObj.map(artist => {
+    const innerHtml = maybeArtistsObj.map<string>(artist => {
       if (!isValidArtistProfile(artist)) return "";
       return `<li>
+        <div></div>
         <h4>${artist.artistName}</h4>
+        <p>
+          ${artist.bio.replace(/\n/g, "<br />")}
+        </p>
         ${artist.links ? `<ul>
           ${artist.links.map(link =>
         `<li>
           <p>
-            ${link.name}: <a href="${link.link}" target="_blank" rel="noopener noreferrer">${link.label}</a>
+            <span>[ ${link.name} ]</span>
+            <span><a href="${link.link}" target="_blank" rel="noopener noreferrer">${link.label}</a></span>
           </p>
         </li>`
       ).join("")}
